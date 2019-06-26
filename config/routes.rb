765fcfdb0a0a264aa211d62ee6904778d5bca1c4
contactnikitakazakov@gmail.com <aaaaaa1111112020>
2019-06-25 20:20:13 -0400

    add forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get '/events' => 'events#index'
+  #get 'events' => 'events#index'
+  #post 'events/' => 'events#create'
+  #get 'events/new' => 'events#new', as: 'new_event'
+  #get 'events/:id/edit' => 'events#edit', as:'edit_event'
+  #get 'events/:id' => 'events#show', as:'event'
+  #patch 'events/:id' => 'events#update'
+
+  resources :events
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-    Event.create(name:"Hackathon", location:"Denver", price: 50)
-    Event.create(name:"Katacamp", location:"NYC", price: 70)
-    Event.create(name:"Javascript Hut", location:"Denver", price: 100)
+    Event.create(name:"KataCamp", location:"Denver", price: 50, starts_at:"2019-06-25 21:56:19", description: "During this talk, we'll cover the emergence of big data and what it means to be a data scientist. We'll walk you through how to get the skills to become a data scientist, and focus on the different jobs that will be open to you once you've mastered your new skill set.")
+    Event.create(name:"HackAthon", location:"NYC", price: 70, description: "Together we'll learn key jQuery concepts like manipulating the DOM and using event listeners to update our app in real time. Here is an example of a web app that was made by an attendee at one of our previous events")
+    Event.create(name:"Javascript Hut", location:"Denver", price: 0, description: "Best event in the world, didn't you know?")
